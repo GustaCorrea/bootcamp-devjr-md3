f340325eb955b066afae1e6eaed1777ad64d7f74
Gustavo Correa <gustavocorrea.1910@gmail.com>
2025-08-25 23:28:00 -0300

    javascript

---
+++ trafficlight.js
@@ -0,0 +1,12 @@
+function changeTrafficLight() {
+  if (document.getElementById("red").className === "light red-light") {
+    document.getElementById("red").className = "light light-off";
+    document.getElementById("yellow").className = "light yellow-light";
+  } else if (document.getElementById("yellow").className === "light yellow-light") {
+    document.getElementById("yellow").className = "light light-off";
+    document.getElementById("green").className = "light green-light";
+  } else if (document.getElementById("green").className === "light green-light") {
+    document.getElementById("green").className = "light light-off";
+    document.getElementById("red").className = "light red-light";
+  }
+}
